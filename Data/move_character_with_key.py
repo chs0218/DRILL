378322bb6dbaa6a36874f2ce900b6d39b853f8a5
Choi Hae Seong <39898936+chs0218@users.noreply.github.com>
2021-10-05 18:04:30 +0900

    drill 07 시작

--- Data/move_character_with_key.py
+++ Data/move_character_with_key.py
@@ -3,25 +3,27 @@ from pico2d import *
 
 def handle_events():
     global running
-    global ToRight
     global dir
+    global frameline
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                ToRight = True
+                frameline = 1
                 dir += 1
             elif event.key == SDLK_LEFT:
-                ToRight = False
+                frameline = 0
                 dir -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
+                frameline = 3
                 dir -= 1
             elif event.key == SDLK_LEFT:
+                frameline = 2
                 dir += 1
     pass
 
@@ -31,25 +33,22 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
 running = True
-ToRight = True
 
 x = 800 // 2
 frame = 0
-dir = 0 # -1 left, +1 right
+frameline = 3
+dir = 0  # -1 left, +1 right
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    if ToRight:
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
-    else:
-        character.clip_draw(frame * 100, 100 * 0, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 100 * frameline, 100, 100, x, 90)
     update_canvas()
 
     handle_events()
     frame = (frame + 1) % 8
     x += dir * 5
-    delay(0.01)
+    delay(0.05)
 
 close_canvas()
 
