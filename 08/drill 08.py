e55f90a7a72bef5b91557e2e2d4417adf8fead9f
Choi Hae Seong <39898936+chs0218@users.noreply.github.com>
2021-10-08 01:12:31 +0900

    drill 08 가지고 놀기

--- 08/drill 08.py
+++ 08/drill 08.py
@@ -49,17 +49,44 @@ def draw_point(p):
     turtle.goto(p)
     turtle.dot(5, random.random(), random.random(), random.random())
 
+def draw_line(p):
+    turtle.pendown()
+    turtle.goto(p)
+
+def draw_k(a, b, n):
+    for i in range(0, 360 * n, 8):
+        t = math.radians(i)
+        x = abs(a - b) * math.cos(t) + b * math.cos(t * (a / b - 1))
+        y = abs(a - b) * math.sin(t) - b * math.sin(t * (a / b - 1))
+        draw_point((x, y))
+    pass
+
+def draw_butterfly():
+    for i in range(0, 360 * 12, 4):
+        t = math.radians(i)
+        x = math.sin(t) * ((math.exp(math.cos(t))) - 2 * math.cos(4 * t) - (math.sin(t/12) ** 5))
+        y = math.cos(t) * ((math.exp(math.cos(t))) - 2 * math.cos(4 * t) - (math.sin(t/12) ** 5))
+        draw_point((x * 100, y * 100))
+    pass
+
+def draw_jk(a, b, c, d, j, k):
+    for i in range(0, 36000 * 28, 1):
+        t = math.radians(i / 100)
+        x = math.cos(a * t) - (math.cos(b * t) ** j)
+        y = math.sin(c * t) - (math.sin(d * t) ** k)
+        draw_line((x * 100, y * 100))
+    pass
 
 prepare_turtle_canvas()
 
 # draw_line_basic(p1, p2)
 
-n = 10
+n = 20
+a = 65
+b = 100
 
-for i in range(0, 360 * n, 8):
-    t = math.radians(i)
-    x = 7 * math.cos(t) + 10 * math.cos(t * 0.7)
-    y = 7 * math.sin(t) - 10 * math.sin(t * 0.7)
-    draw_point((-x * 20, -y * 20))
+# draw_k(a, b, n)
+# draw_butterfly()
+draw_jk(2, 200, 200, 1, 3, 4)
 
 turtle.done()
