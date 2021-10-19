367632b35fb31c3e5e680d082a9202ea3b8d6e57
chs0218 <39898936+chs0218@users.noreply.github.com>
2021-10-19 17:59:16 +0900

    drill 10 완성
    https://youtu.be/UUiwoqHC3aA

--- 10/drill 10.py
+++ 10/drill 10.py
@@ -16,7 +16,24 @@ class Ball:
     def __init__(self):
         self.x = random.randint(0, 599)
         self.y = 599
-        self.image = load_image('ball21x21.png')
+        self.speed = random.randint(5, 10)
+        self.key = random.randint(0, 1)
+        if self.key == 0:
+            self.image = load_image('ball21x21.png')
+        else:
+            self.image = load_image('ball41x41.png')
+
+    def falling(self):
+        if self.key == 0:
+            if self.y > 70:
+                self.y -= self.speed
+            else:
+                self.y = 70
+        else:
+            if self.y > 80:
+                self.y -= self.speed
+            else:
+                self.y = 80
 
     def draw(self):
         self.image.draw(self.x, self.y - 10)
@@ -24,10 +41,14 @@ class Ball:
 
 running = True
 open_canvas(600, 600)
+grass = load_image('grass.png')
 
-Balls = [Ball() for i in range(10)]
+Balls = [Ball() for i in range(20)]
 while running:
+    clear_canvas()
+    grass.draw(300, 30)
     for i in range(10):
+        Balls[i].falling()
         Balls[i].draw()
     update_canvas()
     handle_events()
